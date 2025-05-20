59f30b6b806411dd004494c0171dd0d515a1d17a
Aryan Gaire <botx711@gmail.com>
2025-05-20 13:18:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,6 @@ from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import os
 from char_segment import process_uploaded_image
-import threading
-import zipfile
-import requests
 
 
 app = Flask(__name__)
@@ -25,37 +22,12 @@ def process_image():
     return jsonify({'processed_images': processed_images})
 
 
-def zip_and_send_uploads():
-    zip_path = os.path.join(UPLOAD_FOLDER, "uploads.zip")
-    # Create ZIP
-    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(UPLOAD_FOLDER):
-            for file in files:
-                if file != "uploads.zip":
-                    file_path = os.path.join(root, file)
-                    arcname = os.path.relpath(file_path, UPLOAD_FOLDER)
-                    zipf.write(file_path, arcname)
-    # Send ZIP to receiver backend
-    receiver_url = "https://betadeep.onrender.com/receive-uploads"  # <-- CHANGE THIS
-    with open(zip_path, 'rb') as f:
-        files = {'file': (os.path.basename(zip_path), f, 'application/zip')}
-        try:
-            requests.post(receiver_url, files=files, timeout=30)
-            print("Uploads directory sent....")
-
-        except Exception as e:
-            print(f"Error sending uploads.zip to receiver backend: {e}")
-
-
-
 
 @app.route('/get-image', methods=['GET'])
 def get_image():
     image_path = request.args.get('path')
     if not image_path or not os.path.exists(image_path):
         return jsonify({'error': 'Image not found'}), 404
-
-    threading.Thread(target=zip_and_send_uploads).start()
 
     return send_file(image_path, mimetype='image/png')
 
